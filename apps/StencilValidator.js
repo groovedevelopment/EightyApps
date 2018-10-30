04c07325f770a8bc6b89f01364cb17e34730e435
ccherupil <cherupil@groovecommerce.com>
2018-10-30 10:08:40 -0400

    Added stencil checker

---
+++ apps/StencilValidator.js
@@ -0,0 +1,41 @@
+var EightyApp = function() {
+  	// processDocument returns scraped data
+    this.processDocument = function(html, url, headers, status, cheerio) {
+        $ = cheerio;
+        var app = this;
+        var $html = app.parseHtml(html, cheerio);
+        var object = new Object();
+
+        var title = $html.find('title').first().text();
+        var stencil = false;
+        $html.find('[data-stencil-stylesheet]').each(function(i, obj){
+            stencil = true;
+        })
+        
+        object.url = url;
+        object.title = title;
+        object.stencil = stencil;
+
+        return JSON.stringify(object);
+    }
+
+    // parseLinks returns the next set of links to crawl
+    this.parseLinks = function(html, url, headers, status, cheerio) {
+        var app = this;
+        var $html = app.parseHtml(html, cheerio);
+        var links = [];
+
+        // generate the set of links you want to crawl next
+
+        return links;
+    }
+}
+
+try {
+    module.exports = function(EightyAppBase) {
+        EightyApp.prototype = new EightyAppBase();
+        return new EightyApp();
+    }
+} catch(e) {
+    EightyApp.prototype = new EightyAppBase();
+}
